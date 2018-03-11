87571cacf8a23192d663dd0e27af77c23184a535
tiwAstha <astha.tiwari.usu@gmail.com>
2018-03-11 01:51:00 -0700

    commit1

---
+++ MessengerBuddy/src/main/java/org/astha/messenger/database/DatabaseClass.java
@@ -0,0 +1,26 @@
+package org.astha.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.astha.messenger.model.Message;
+import org.astha.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<String, Profile> getProfiles() {
+		return profiles;
+	}
+
+	
+	
+	
+}
---
+++ MessengerBuddy/src/main/java/org/astha/messenger/exception/GenericExceptionMapper.java
@@ -0,0 +1,23 @@
+package org.astha.messenger.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.astha.messenger.model.ErrorMessage;
+
+// This class intentionally doesn't have the @Provider annotation.
+// It has been disabled in order to try out other ways of throwing exceptions in JAX-RS
+
+// @Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+
+	@Override
+	public Response toResponse(Throwable ex) {
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 500, "https://github.com/tiwAstha");
+		return Response.status(Status.INTERNAL_SERVER_ERROR)
+				.entity(errorMessage)
+				.build();
+	}
+
+}
